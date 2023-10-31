740bffbb91af6fd786ffa49cc89e1f8fc135c73b
Eliassandoval23 <eliassandoval2005@gmail.com>
2023-10-31 17:26:58 -0400

    test

--- encoder.py
+++ encoder.py
@@ -15,6 +15,14 @@ def encodePassword(password):
     
     return newPassword
 
+def decodePassword(password):
+    newPassword = ""
+    for digit in password:
+        newDigit = int(digit) - 3
+        newPassword += str(newDigit)
+
+    return newPassword
+
 
 def main():
     while True:
